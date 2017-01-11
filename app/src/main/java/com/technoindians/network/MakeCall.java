a1dbd80ee769186fd03f170a14dc6b92a24c16ec
Girish <girish@sagesurfer.com>
2017-01-11 18:27:04 +0530

    Code optimization

--- app/src/main/java/com/technoindians/network/MakeCall.java
+++ app/src/main/java/com/technoindians/network/MakeCall.java
@@ -14,22 +14,19 @@ import okio.Buffer;
  * @author
  * Girish Mane <girishmane8692@gmail.com>
  * Created on 25/04/2016
- * Last modified 30/07/2016
+ * Last modified 11/01/2017
  *
  */
 public class MakeCall {
+    private static OkHttpClient client = new OkHttpClient();
 
-    private static final String TAG = MakeCall.class.getSimpleName();
-    static OkHttpClient client = new OkHttpClient();
-
-    public static String post(String url,RequestBody formBody) throws Exception {
+    public static String post(String url,RequestBody formBody,String TAG) throws Exception {
         Request request = new Request.Builder()
                 .url(url)
                 .post(formBody)
                 .build();
 
         Response response = client.newCall(request).execute();
-       // if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
         String res = response.body().string();
         Log.e(TAG,"Url -> "+url+"?"+bodyToString(request)+"\nResponse -> "+res);
         if (response.isSuccessful()){
@@ -38,21 +35,12 @@ public class MakeCall {
             return null;
         }
     }
-    public static String dummyPost(String url,RequestBody formBody) throws Exception {
+    public static String dummyPost(String url,RequestBody formBody,String TAG) throws Exception {
         Request request = new Request.Builder()
                 .url(url)
                 .post(formBody)
                 .build();
         Log.e(TAG,"dummyPost() Url -> "+url+"?"+bodyToString(request));
-        Response response = null;// client.newCall(request).execute();
-        // if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-        //String res = response.body().string();
-        /*System.out.print(res);
-        if (response.isSuccessful()){
-            return res;
-        }else {
-            return null;
-        }*/
         return null;
     }
     private static String bodyToString(final Request request){
