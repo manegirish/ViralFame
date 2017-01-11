29fb1a233ce4f133f40356acc43ab621fe0ad200
Girish <girish@sagesurfer.com>
2017-01-11 19:02:23 +0530

    No Data warnign removed

--- app/src/main/java/com/technoindians/wall/WallCommentDialogFragment.java
+++ app/src/main/java/com/technoindians/wall/WallCommentDialogFragment.java
@@ -43,13 +43,12 @@ import okhttp3.FormBody;
 import okhttp3.RequestBody;
 
 /**
- * @author
- * Girish Mane (girishmane8692@gmail.com)
- * created on 9/7/16
- * last modified 11/01/17
+ * @author Girish Mane (girishmane8692@gmail.com)
+ *         created on 9/7/16
+ *         last modified 11/01/17
  */
 
-public class WallCommentDialogFragment extends DialogFragment implements View.OnClickListener{
+public class WallCommentDialogFragment extends DialogFragment implements View.OnClickListener {
 
     private static final String TAG = WallCommentDialogFragment.class.getSimpleName();
 
@@ -69,7 +68,7 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         @SuppressLint("InflateParams")
-        View view = inflater.inflate(R.layout.commet_dialog_layout,null);
+        View view = inflater.inflate(R.layout.commet_dialog_layout, null);
 
         activity = getActivity();
         updateOperations = new UpdateOperations(activity.getApplicationContext());
@@ -78,45 +77,45 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
         commentList = new ArrayList<>();
 
         Bundle bundle = getArguments();
-        if (bundle!=null){
-            wall_id =  bundle.getString(Constants.ID);
+        if (bundle != null) {
+            wall_id = bundle.getString(Constants.ID);
         }
 
-        listView = (ListView)view.findViewById(R.id.comment_dialog_list_view);
-        warning = (TextView)view.findViewById(R.id.comment_dialog_warning);
+        listView = (ListView) view.findViewById(R.id.comment_dialog_list_view);
+        warning = (TextView) view.findViewById(R.id.comment_dialog_warning);
 
-        backButton = (ImageView)view.findViewById(R.id.comment_dialog_back);
+        backButton = (ImageView) view.findViewById(R.id.comment_dialog_back);
         backButton.setOnClickListener(this);
 
-        sendButton = (ImageView)view.findViewById(R.id.comment_dialog_send);
+        sendButton = (ImageView) view.findViewById(R.id.comment_dialog_send);
         sendButton.setOnClickListener(this);
 
-        likeListButton = (RelativeLayout)view.findViewById(R.id.comment_dialog_like_layout);
+        likeListButton = (RelativeLayout) view.findViewById(R.id.comment_dialog_like_layout);
         likeListButton.setOnClickListener(this);
 
-        messageBox = (EditText)view.findViewById(R.id.comment_dialog_box);
+        messageBox = (EditText) view.findViewById(R.id.comment_dialog_box);
 
-        commentListAdapter = new CommentListAdapter(activity,commentList);
+        commentListAdapter = new CommentListAdapter(activity, commentList);
         listView.setAdapter(commentListAdapter);
 
-        setWarning(Warnings.NO_DATA,R.drawable.ic_no_data);
+        setWarning(Warnings.LOADING, R.drawable.ic_data);
         return view;
     }
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState)
-    {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.getWindow().getAttributes().windowAnimations = R.style.dialog_animation_up;
         return dialog;
     }
 
-    private void setWarning(String message,int image){
+    private void setWarning(String message, int image) {
         listView.setVisibility(View.GONE);
         warning.setVisibility(View.VISIBLE);
         warning.setText(message);
         warning.setCompoundDrawablesWithIntrinsicBounds(0, image, 0, 0);
     }
+
     @Override
     public void onStart() {
         super.onStart();
@@ -152,29 +151,29 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.comment_dialog_back:
                 dismiss();
                 break;
             case R.id.comment_dialog_send:
                 String message = messageBox.getText().toString().trim();
-                if (Validator_.comment(message)==1){
+                if (Validator_.comment(message) == 1) {
                     new Comment().execute(message);
-                }else {
+                } else {
                     messageBox.setError(Warnings.INVALID_COMMENT);
                 }
                 break;
             case R.id.comment_dialog_like_layout:
-                Intent listIntent = new Intent(activity.getApplicationContext(),LikedListActivity.class);
+                Intent listIntent = new Intent(activity.getApplicationContext(), LikedListActivity.class);
                 Bundle nextAnimation = ActivityOptions.makeCustomAnimation
-                        (activity.getApplicationContext(), R.anim.animation_one,R.anim.animation_two).toBundle();
-                listIntent.putExtra(Constants.WALL_ID,wall_id);
-                startActivity(listIntent,nextAnimation);
+                        (activity.getApplicationContext(), R.anim.animation_one, R.anim.animation_two).toBundle();
+                listIntent.putExtra(Constants.WALL_ID, wall_id);
+                startActivity(listIntent, nextAnimation);
                 break;
         }
     }
 
-    private class Comment extends AsyncTask<String,Void,Integer>{
+    private class Comment extends AsyncTask<String, Void, Integer> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -190,16 +189,16 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
                     .add(Constants.ACTION, Actions_.COMMENT)
                     .build();
             try {
-                String response = MakeCall.post(Urls.DOMAIN + Urls.POST_OPERATIONS_URL,requestBody,TAG);
-                if (response!=null){
+                String response = MakeCall.post(Urls.DOMAIN + Urls.POST_OPERATIONS_URL, requestBody, TAG);
+                if (response != null) {
                     JSONObject jsonObject = new JSONObject(response);
-                    if (jsonObject.has(JsonArrays_.COMMENT)){
+                    if (jsonObject.has(JsonArrays_.COMMENT)) {
                         JSONObject responseObject = jsonObject.getJSONObject(JsonArrays_.COMMENT);
                         result = responseObject.getInt(Constants.STATUS);
-                    }else {
+                    } else {
                         result = 12;
                     }
-                }else {
+                } else {
                     result = 12;
                 }
             } catch (Exception e) {
@@ -212,33 +211,33 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            switch (integer){
-                    case 0:
-                        ShowToast.actionFailed(activity.getApplicationContext());
-                        break;
-                    case 1:
-                        messageBox.setText("");
-                        ShowToast.successful(activity.getApplicationContext());
-                        new FetchComments().execute();
-                        break;
-                    case 2:
-                        ShowToast.actionFailed(activity.getApplicationContext());
-                        break;
-                    case 11:
-                        ShowToast.networkProblemToast(activity.getApplicationContext());
-                        break;
-                    case 12:
-                        ShowToast.internalErrorToast(activity.getApplicationContext());
-                        break;
+            switch (integer) {
+                case 0:
+                    ShowToast.actionFailed(activity.getApplicationContext());
+                    break;
+                case 1:
+                    messageBox.setText("");
+                    ShowToast.successful(activity.getApplicationContext());
+                    new FetchComments().execute();
+                    break;
+                case 2:
+                    ShowToast.actionFailed(activity.getApplicationContext());
+                    break;
+                case 11:
+                    ShowToast.networkProblemToast(activity.getApplicationContext());
+                    break;
+                case 12:
+                    ShowToast.internalErrorToast(activity.getApplicationContext());
+                    break;
             }
         }
     }
 
-    private class FetchComments extends AsyncTask<Void,Void,Integer>{
+    private class FetchComments extends AsyncTask<Void, Void, Integer> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            setWarning("Loading..... ",R.drawable.ic_no_data);
+            setWarning("Loading..... ", R.drawable.ic_no_data);
         }
 
         @Override
@@ -250,15 +249,15 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
                     .add(Constants.ID, wall_id)
                     .add(Constants.ACTION, Actions_.GET_COMMENT)
                     .build();
-           // Log.e(TAG,"------>");
+            // Log.e(TAG,"------>");
             try {
-                String response = MakeCall.post(Urls.DOMAIN + Urls.POST_OPERATIONS_URL,requestBody,TAG);
-                if (response!=null){
+                String response = MakeCall.post(Urls.DOMAIN + Urls.POST_OPERATIONS_URL, requestBody, TAG);
+                if (response != null) {
                     result = Wall_.commentResult(response);
-                    if (result==1){
+                    if (result == 1) {
                         commentList = Wall_.parseComment(response);
                     }
-                }else {
+                } else {
                     result = 12;
                 }
             } catch (Exception e) {
@@ -271,29 +270,27 @@ public class WallCommentDialogFragment extends DialogFragment implements View.On
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            switch (integer){
+            switch (integer) {
                 case 0:
-                    ShowToast.noData(activity.getApplicationContext());
-                    setWarning(Warnings.NO_DATA,R.drawable.ic_no_data);
+                    warning.setVisibility(View.GONE);
                     break;
                 case 1:
                     commentListAdapter.clear();
                     listView.setVisibility(View.VISIBLE);
                     warning.setVisibility(View.GONE);
-                    commentListAdapter = new CommentListAdapter(activity,commentList);
+                    commentListAdapter = new CommentListAdapter(activity, commentList);
                     listView.setAdapter(commentListAdapter);
                     updateOperations.updateFeed(wall_id, Constants.TOTAL_COMMENTS, "" + commentList.size());
                     break;
                 case 2:
-                    ShowToast.noData(activity.getApplicationContext());
-                    setWarning(Warnings.NO_DATA,R.drawable.ic_no_data);
+                    warning.setVisibility(View.GONE);
                     break;
                 case 11:
                     ShowToast.networkProblemToast(activity.getApplicationContext());
-                    setWarning(Warnings.NETWORK_ERROR_WARNING,R.drawable.ic_network_problem);
+                    setWarning(Warnings.NETWORK_ERROR_WARNING, R.drawable.ic_network_problem);
                     break;
                 case 12:
-                    setWarning(Warnings.INTERNAL_ERROR_WARNING,R.drawable.ic_network_problem);
+                    setWarning(Warnings.INTERNAL_ERROR_WARNING, R.drawable.ic_network_problem);
                     ShowToast.internalErrorToast(activity.getApplicationContext());
                     break;
             }
